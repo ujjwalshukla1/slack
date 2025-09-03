1b529bdc6aff145a8affd5a1a95d2e62438bb1e7
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-09-03 23:34:03 +0530

    db-connected & env.js completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
-    res.send("Hello world!")
-})
-console.log("mongo_uri:", ENV.MONGO_URI)
-app.listen(ENV.PORT, () => console.log("Server started on the port:", ENV.PORT))
+  res.send("Hello world!");
+});
+
+app.listen(ENV.PORT, () => {
+  console.log("Server started on the port:", ENV.PORT);
+  connectDB();
+});
