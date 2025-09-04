c0164f1c29fe049d872efa161f8337f3a74f2377
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-09-04 23:27:01 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,34 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { inngest, functions } from "./config/ingest.js";
+import { serve } from "inngest/express";
 
 const app = express();
+app.use(clerkMiddleware());
+app.use(express.json());
 
-const PORT = process.env.PORT || 5001;
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.listen(ENV.PORT, () => {
-  console.log("Server started on the port:", ENV.PORT);
-  connectDB();
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV === "Production") {
+      app.listen(ENV.PORT, () => {
+        console.log(
+          `Server running in ${ENV.NODE_ENV} mode on port ${ENV.PORT}`
+        );
+      });
+    }
+  } catch (error) {
+    console.log("Error starting server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
