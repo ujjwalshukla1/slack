021aca7ce69f533a16de04cc838a04c87fb7a678
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-09-04 23:34:02 +0530

    small fixes for ingest.js file

--- backend/src/config/ingest.js
+++ backend/src/config/ingest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
+import {User} from "../models/user.model.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
@@ -30,4 +31,4 @@ const deleteUser = inngest.createFunction(
   }
 );
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [syncUser, deleteUser];
