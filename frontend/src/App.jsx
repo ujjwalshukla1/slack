31d52911ddfa4b84e1bc660c357edba18503d7d5
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-09-09 23:27:13 +0530

    useStreamChat added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,51 @@
 import React from "react";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
+import { useAuth, UserButton } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 import * as Sentry from "@sentry/react";
+import CallPage from "./pages/CallPage";
 
 const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
 const App = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  } 
   return (
     <>
-      <SignedIn>
-        <SentryRoutes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<Navigate to={"/"} replace />} />
-        </SentryRoutes>
-        <UserButton />
-      </SignedIn>
-      <SignedOut>
-        {/* <SignInButton mode="modal" /> */}
-        <SentryRoutes>
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="*" element={<Navigate to={"/auth"} replace />} />
-        </SentryRoutes>
-      </SignedOut>
+      <UserButton />
+      <SentryRoutes>
+        <Route
+          path="/"
+          element={
+            isSignedIn ? <HomePage /> : <Navigate to={"/auth"} replace />
+          }
+        />
+        <Route
+          path="/auth"
+          element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+        />
+        <Route
+          path="/call/:id"
+          element={
+            isSignedIn ? <CallPage /> : <Navigate to={"/auth"} replace />
+          }
+        />
+
+        <Route
+          path="*"
+          element={
+            isSignedIn ? (
+              <Navigate to={"/"} replace />
+            ) : (
+              <Navigate to={"/auth"} replace />
+            )
+          }
+        />
+      </SentryRoutes>
     </>
   );
 };
